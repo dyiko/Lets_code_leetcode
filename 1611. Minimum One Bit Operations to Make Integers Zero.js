fed94ac6ab16dd9ebd03021cd1a110a8458d64a0
Ayushman.G <140804930+dyiko@users.noreply.github.com>
2023-11-30 21:21:14 +0530

    Add files via upload

---
+++ 1611. Minimum One Bit Operations to Make Integers Zero.js
@@ -0,0 +1,23 @@
+//1611. Minimum One Bit Operations to Make Integers Zero
+// hardest quetion of november solved using dp
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var minimumOneBitOperations = function(n) {
+    if (n === 0) {
+        return 0;
+    }
+
+    let k = 0;
+    let curr = 1;
+
+    while (curr * 2 <= n) {
+        curr *= 2;
+        k++;
+    }
+
+    return Math.pow(2, k + 1) - 1 - minimumOneBitOperations(n ^ curr);
+
+};
