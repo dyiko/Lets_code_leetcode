913822e51a6a947d4aa40deed646bb16a10a8c4e
Ayushman.G <140804930+dyiko@users.noreply.github.com>
2023-12-15 20:59:08 +0530

    Add files via upload

---
+++ 1436. Destination City.js
@@ -0,0 +1,17 @@
+//1436. Destination City
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+var destCity = function(paths) {
+    const set = new Set();
+    for (const path of paths){
+        set.add(path[0]);
+    }
+    for (const path of paths){
+        if (!set.has(path[1])){
+            return path[1];
+        }
+    }
+    return null;
+};
