e9b53315fe9f6504b2b20b1cda848a8eab9146e7
Ayushman.G <140804930+dyiko@users.noreply.github.com>
2024-01-07 22:22:37 +0530

    Add files via upload

---
+++ 446. Arithmetic Slices II - Subsequence.js
@@ -0,0 +1,24 @@
+//446. Arithmetic Slices II - Subsequence
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numberOfArithmeticSlices = function(nums) {
+    const n = nums.length;
+    let ans = 0;
+    const dp = new Array(n).fill().map(() => new Map());
+
+    for (let i = 1; i < n; i++) {
+      for (let j = 0; j < i; j++) {
+        const diff = nums[i] - nums[j];
+        if (dp[j].has(diff)) {
+                dp[i].set(diff, (dp[i].get(diff) || 0) + dp[j].get(diff));
+                ans += dp[j].get(diff);
+            }
+
+            dp[i].set(diff, (dp[i].get(diff) || 0) + 1);
+        }
+    }
+
+    return ans;
+};
