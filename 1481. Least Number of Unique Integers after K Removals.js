38a2a439c978362a540b9ef3fb44437a92c46f6a
Ayushman.G <140804930+dyiko@users.noreply.github.com>
2024-02-16 19:48:16 +0530

    Add files via upload

---
+++ 1481. Least Number of Unique Integers after K Removals.js
@@ -0,0 +1,20 @@
+//1481. Least Number of Unique Integers after K Removals
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findLeastNumOfUniqueInts = function(arr, k) {
+    const seen={}
+    for(let num of arr){
+        seen[num]=(seen[num]||0)+1
+    }
+    const nums =Object.entries(seen).sort((a,b)=>b[1]-a[1])
+    while(k>0){
+        const count=nums[nums.length-1][1]
+        if(k<count) break
+        k-=count
+        nums.pop()
+    }
+    return nums.length
+};
