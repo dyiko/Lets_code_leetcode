600c52996028304d2df47e94362c29ba6aa128d3
Ayushman.G <140804930+dyiko@users.noreply.github.com>
2024-02-21 02:15:24 +0530

    Add files via upload

---
+++ 268. Missing Number.js
@@ -0,0 +1,14 @@
+//268. Missing Number
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n=nums.length+1;
+    let total=(n*(n-1))/2;
+    for (let num of nums){
+        total-=num
+    }
+    return total;
+        
+};
