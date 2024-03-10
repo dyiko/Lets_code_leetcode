16ff3568bd32b67aae1b30483306fde02d4e7bd5
Ayushman.G <140804930+dyiko@users.noreply.github.com>
2024-03-10 19:34:38 +0530

    Add files via upload

---
+++ 349. Intersection of Two Arrays.js
@@ -0,0 +1,15 @@
+//349. Intersection of Two Arrays
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let arr=[]
+    for(let num of nums1){
+        if(nums2.includes(num) && !arr.includes(num)){
+            arr.push(num)
+        }
+    }
+    return arr
+};
