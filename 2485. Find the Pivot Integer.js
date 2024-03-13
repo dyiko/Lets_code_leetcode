093acfa518975f32a719040b7231dbba9d62128c
Ayushman.G <140804930+dyiko@users.noreply.github.com>
2024-03-13 21:19:34 +0530

    Add files via upload

---
+++ 2485. Find the Pivot Integer.js
@@ -0,0 +1,14 @@
+//2485. Find the Pivot Integer
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var pivotInteger = function(n) {
+    let x = Math.sqrt(n * (n + 1) / 2);
+    if (x % 1 !== 0) {
+        return -1;
+    } 
+    else {
+        return Math.floor(x);
+    }
+};
