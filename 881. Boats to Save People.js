8aa60fd4ea1943d887aa0e0e14d02c4ea84eaadf
Ayushman.G <140804930+dyiko@users.noreply.github.com>
2024-05-05 00:24:48 +0530

    Add files via upload

---
+++ 881. Boats to Save People.js
@@ -0,0 +1,22 @@
+881. Boats to Save People
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+    people.sort((a,b)=>a-b);
+    let boats =0;
+    let l=0,r=people.length-1;
+    while(l<=r){
+        if (people[l]+people[r]<=limit){
+            l++
+            r--
+        }
+        else{
+            r--
+        }
+        boats++
+    }
+    return boats;
+};
